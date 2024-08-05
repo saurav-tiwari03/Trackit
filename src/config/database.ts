98da19cb77f5933c4e45ecb2dc8e920b46a30a2f
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-08-05 17:01:08 +0530

    database update

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connect() {
   try {
-    const connection = await mongoose.connect('mongodb://localhost:27017/trackit'); 
+    const connection = await mongoose.connect(process.env.MONGODB_URI!); 
     console.log('Connected to MongoDB');
   } catch (error) {
     console.log('Error connecting to MongoDB')
